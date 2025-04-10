1210b9926bfe02096249d1362132fe1c5531ddb9
freeliujian <670177821@qq.com>
2025-04-10 14:25:00 +0800

    feat: 添加一个em过滤标签和新增reqwest tokio两个包

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@ use std::path;
 use std::fs::read_to_string;
 use std::env::Args;
 use regex::Regex;
+use reqwest;
 
 pub struct ArticleArgsParams {
     pub paths: String,
@@ -10,6 +11,10 @@ pub struct ArticleArgsParams {
 
 impl ArticleArgsParams {
      pub fn handle_article(location: ArticleArgsParams) -> Vec<String> {
+         if location.num > 32  {
+             panic!("Invalid article number: {}", location.num);
+         }
+
         let path = path::Path::new(location.paths.as_str());
         let regx = Regex::new(r"[\r\n]+").unwrap();
         let num = location.num;
@@ -41,6 +46,27 @@ pub fn get_args_params(mut arg: Args) ->(String, usize) {
     (path, num)
 }
 
+pub fn get_file_end_name (file_name: &str) -> String {
+    let file_path = path::Path::new(file_name);
+    let extension = file_path.extension().and_then(|s| s.to_str());
+    extension.unwrap().to_string()
+}
+
+pub async fn get(url: &str) -> Result<String, reqwest::Error> {
+    let response = reqwest::get(url).await?;
+    response.text().await
+}
+
+pub async fn get_request_search_engineer()  {
+    let test = match get("http://www.baidu.com/s?wd=%E6%88%91%E6%9C%89%E4%B8%80%E4%B8%AA%E6%A2%A6%E6%83%B3").await {
+        Ok(res) => res,
+        Err(e) => panic!("{}", e),
+    };
+    let filter_em_regex = Regex::new(r"<em\b[^>]*>(.*?)</em>");
+    let filter_em_regex_pin = filter_em_regex.unwrap().replace_all(&test, "").to_string();
+    println!("test is :{:?}", test);
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -53,5 +79,15 @@ mod tests {
     #[test]
     fn test_get_arg_params() {
 
+    }
+
+    #[test]
+    fn test_get_file_end_name() {
+        get_file_end_name("readme.md");
+        assert_eq!("md", get_file_end_name("./../../README.md"));
+    }
+
+    #[test]
+    fn test_get_request_search_engineer() {
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use check_article_review::{get_args_params,ArticleArgsParams};
-use std::env;
-fn main() {
-    let args = env::args();
-    let (path, num) = get_args_params(args);
-    let article_content_vec = ArticleArgsParams::handle_article(
-        ArticleArgsParams{
-            paths: path,
-            num
-        }
-    );
+use check_article_review::{get_request_search_engineer};
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    // let args = env::args();
+    // let (path, num) = get_args_params(args);
+    // let article_content_vec = ArticleArgsParams::handle_article(
+    //     ArticleArgsParams{
+    //         paths: path,
+    //         num
+    //     }
+    // );
+    get_request_search_engineer().await;
 }
