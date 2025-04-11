edbf588f0be68bd00c71cd0a6acda85642c78e48
freeliujian <670177821@qq.com>
2025-04-11 15:36:38 +0800

    feat: 添加feture包进行并行请求

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::fs::read_to_string;
 use std::env::Args;
 use regex::Regex;
 use reqwest;
+use futures::future::join_all;
 
 
 
@@ -11,7 +12,47 @@ pub struct ArticleArgsParams {
     pub num: usize,
 }
 
+impl ArticleArgsParams {
+    pub fn handle_article(location: ArticleArgsParams) -> Vec<String> {
+        if location.num > 32  {
+            panic!("Invalid article number: {}", location.num);
+        }
+        let path = path::Path::new(location.paths.as_str());
+        let regx = Regex::new(r"[\r\n]+").unwrap();
+        let num = location.num;
+        let article_content = read_to_string(path).unwrap();
+        let article_content_format = article_content
+            .trim()
+            .replace("\n\r", "")
+            .replace("\n", "")
+            .replace("\r", "")
+            .replace(' ', "");
+        let regx_content = regx.replace_all(&article_content_format, "")
+            .chars()
+            .collect::<Vec<char>>()
+            .chunks(num)
+            .map(|chunk| {
+                chunk.iter().collect::<String>()
+            })
+            .collect::<Vec<String>>();
+        regx_content
+    }
+}
+
 pub struct GetRequestSearchEngineer {
+    pub url:SearchEngineer,
+    pub search_content:String,
+}
+
+impl GetRequestSearchEngineer {
+    pub async fn get(search_params: GetRequestSearchEngineer) -> Result<String, reqwest::Error>  {
+        let get_url = match search_params.url {
+            SearchEngineer::Baidu(url) => url + search_params.search_content.as_ref(),
+            SearchEngineer::None => panic!("No Engineer!"),
+        };
+        let response = reqwest::get(get_url).await?;
+        response.text().await
+    }
 }
 
 #[derive(Debug)]
@@ -35,39 +76,6 @@ impl SearchEngineer{
 }
 
 
-impl ArticleArgsParams {
-     pub fn handle_article(location: ArticleArgsParams) -> Vec<String> {
-         if location.num > 32  {
-             panic!("Invalid article number: {}", location.num);
-         }
-        let path = path::Path::new(location.paths.as_str());
-        let regx = Regex::new(r"[\r\n]+").unwrap();
-        let num = location.num;
-        let article_content = read_to_string(path).unwrap();
-        let article_content_format = article_content
-            .trim()
-            .replace("\n\r", "")
-            .replace("\n", "")
-            .replace("\r", "");
-        let regx_content = regx.replace_all(&article_content_format, "")
-            .chars()
-            .collect::<Vec<char>>()
-            .chunks(num)
-            .map(|chunk| {
-                chunk.iter().collect::<String>()
-            })
-            .collect::<Vec<String>>();
-         regx_content
-    }
-}
-
-impl GetRequestSearchEngineer {
-    pub async fn get(url: &str) -> Result<String, reqwest::Error>  {
-        let response = reqwest::get(url).await?;
-        response.text().await
-    }
-}
-
 pub fn get_args_params(mut arg: Args) ->(String, usize) {
     arg.next();
     let path = arg.next().unwrap();
@@ -84,16 +92,35 @@ pub fn get_file_end_name (file_name: &str) -> String {
     extension.unwrap().to_string()
 }
 
-pub async fn get_request_search_engineer()  {
-    let test = match GetRequestSearchEngineer::get("http://www.baidu.com/s?wd=%E6%88%91%E6%9C%89%E4%B8%80%E4%B8%AA%E6%A2%A6%E6%83%B3").await {
-        Ok(res) => res,
-        Err(e) => panic!("{}", e),
-    };
-    let filter_em_regex = Regex::new(r"<em\b[^>]*>(.*?)</em>");
-    let filter_em_regex_pin = filter_em_regex.unwrap().replace_all(&test, "").to_string();
-    println!("test is :{:?}", test);
+pub async fn get_request_search_engineer(content: String) {
+    let filter_em_regex = Regex::new(r"<em>(.*?)</em>");
+    let filter_em_regex_pin:usize = filter_em_regex
+        .unwrap().captures_iter(content.as_str())
+        .filter_map(|captures| captures.get(1).map(|m| m.as_str().to_string()))
+        .collect::<Vec<String>>()
+        .len();
+
+    println!("test is :{:?}", filter_em_regex_pin);
 }
 
+pub async fn loop_request_search(content: Vec<String>, url: SearchEngineer)  {
+    let task = content.into_iter().map(
+         |item| {
+            let url = match &url {
+                SearchEngineer::Baidu(url) => url.as_str(),
+                SearchEngineer::None => panic!("No Engineer!"),
+            };
+            async move {
+                let req =  GetRequestSearchEngineer::get(GetRequestSearchEngineer {
+                    url:SearchEngineer::Baidu(url.to_owned()),
+                    search_content: item,
+                }).await.expect("TODO: panic message");
+                println!("{:?}", req);
+            }
+        }
+    );
+    join_all(task).await;
+}
 
 #[cfg(test)]
 mod tests {
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
-use check_article_review::{get_request_search_engineer,SearchEngineer};
+use std::env;
+use check_article_review::{get_args_params, loop_request_search, ArticleArgsParams, SearchEngineer};
 use tokio;
 
 
 
 #[tokio::main]
 async fn main() {
-    // let args = env::args();
-    // let (path, num) = get_args_params(args);
-    // let article_content_vec = ArticleArgsParams::handle_article(
-    //     ArticleArgsParams{
-    //         paths: path,
-    //         num
-    //     }
-    // );
+    let args = env::args();
+    let (path, num) = get_args_params(args);
+    let article_content_vec = ArticleArgsParams::handle_article(
+        ArticleArgsParams{
+            paths: path,
+            num
+        }
+    );
     let baidu_http = SearchEngineer::new(String::from("baidu"));
-    println!("{:?}",baidu_http);
-   // get_request_search_engineer().await;
+    loop_request_search(article_content_vec,baidu_http).await;
 }
